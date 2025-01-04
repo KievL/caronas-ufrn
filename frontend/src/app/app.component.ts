65168dd406f8c825bc5d185201e0859432300de5
Joao Paulo Oliveira Cabral <joaopaulooc262@gmail.com>
2025-01-04 12:07:04 -0300

    Mobile

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule} from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { BottomSidenavComponent } from "./bottom-sidenav/bottom-sidenav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    BottomSidenavComponent
+],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
