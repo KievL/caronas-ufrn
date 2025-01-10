3437d35612207a62f720ad3bb1fa8a386bb91df0
kievlz <kievluizfg@gmail.com>
2025-01-09 21:34:12 -0300

    feat: creating default user image

--- backend/src/main/resources/db/migrations/V1__Creating_users_table.sql
+++ backend/src/main/resources/db/migrations/V1__Creating_users_table.sql
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS users(
 	role VARCHAR NOT NULL
 );
 
-INSERT INTO users (id, name, email, course, enrollment_number, password_hash,role)
-VALUES ('2dd7fd6e-28e7-44c9-93a0-c97172987f9c', 'admin', 'admin@umbrella.com', 'UFRN', '01', '$2y$10$lWhUB4so2PZciqcwyFCLzu/2eV89zX7ci.Hw0A9wKIRjsB9ryrlWe', 'ADMIN');
+INSERT INTO users (id, name, email, course, enrollment_number, image_url, password_hash,role)
+VALUES ('2dd7fd6e-28e7-44c9-93a0-c97172987f9c', 'admin', 'admin@umbrella.com', 'UFRN', '01', 'https://frightened-purple-gopher.myfilebase.com/ipfs/QmQm1neVUDC2ojuijt6vTmnsyQrJF17famR176fSi67Jph','$2y$10$lWhUB4so2PZciqcwyFCLzu/2eV89zX7ci.Hw0A9wKIRjsB9ryrlWe', 'ADMIN');
