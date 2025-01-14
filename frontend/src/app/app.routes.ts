fa771d0f11c323618b453426cb388d4fc750893e
Kaiky-Hanry <kaikyhanry@gmail.com>
2025-01-14 08:48:54 -0300

    Atualizando arquivos relacionados ao perfil

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { TripAvailableComponent } from './trip-available/trip-available.component';
 import { TripOngoingComponent } from './trip-ongoing/trip-ongoing.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ChatMessengerComponent } from './chat-messenger/chat-messenger.component';
+import { TripHistoryComponent } from './trip-history/trip-history.component';
 
 export const routes: Routes = [
     {
@@ -16,6 +18,14 @@ export const routes: Routes = [
     {
         path:'trip-ongoing',
         component: TripOngoingComponent
+    },
+    {
+        path:'trip-history',
+        component: TripHistoryComponent
+    },
+    {
+        path:'chat-messenger',
+        component: ChatMessengerComponent
     },
     {
         path:'profile',
--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import { MatButtonModule } from '@angular/material/button'
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,MatButtonModule, RouterModule, MatListModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
